8a2b6782e7ffacbbf4681d01b7cb062e57c52a59
clystone <1049267602@qq.com>
2017-07-08 20:46:58 +0800

    游戏架构完成，开始游戏逻辑

--- 2048main.js
+++ 2048main.js
@@ -3,4 +3,58 @@
  */
 var board = new Array();
 var score = 0;
-var test = 0;
+
+$(document).ready(function () {
+    newgame();
+});
+
+function newgame(){
+    //初始化棋盘格
+    init();
+    //在随机的两个格子生成数字
+}
+
+function init(){
+    for (var i = 0; i < 4; i++){
+        for (var j = 0; j < 4; j++){
+            var girdCell = $("#gird-cell-" + i + "-" + j);
+            girdCell.css("top",getPosTop(i,j));
+            girdCell.css("left",getPosLeft(i,j));
+        }
+    }
+
+    for(var i =0; i < 4; i++){
+        board[i] = new Array();
+        for(var j = 0; j < 4; j++){
+            board[i][j] = 0;
+        }
+    }
+
+    updateBoardView();
+}
+
+function updateBoardView(){
+    $(".number-cell").remove();
+    for (var i = 0;i < 4; i++){
+        for (var j = 0; j < 4; j++) {
+            $("#gird-container").append('<div class="number-cell" id="number-ceil-'+i+'-'+j+'"></div>');
+            var theNumberCell = $('#number-cell-'+i+'-'+j);
+
+            if (board[i][j] === 0){
+                theNumberCell.css('width','0px');
+                theNumberCell.css('height','0px');
+                theNumberCell.css('top',getPosTop(i,j)+50);
+                theNumberCell.css('left',getPosLeft(i,j)+50);
+            }
+            else{
+                theNumberCell.css('width','100px');
+                theNumberCell.css('height','100px');
+                theNumberCell.css('top',getPosTop(i,j));
+                theNumberCell.css('left',getPosLeft(i,j));
+                theNumberCell.css('background-color',getNumberBackgroundColor(board[i][j]));
+                theNumberCell.css('color',getNumberColor(board[i][j]));
+                theNumberCell.test(board[i][j]);
+            }
+        }
+    }
+}
