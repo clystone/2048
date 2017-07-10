9dd385ae53034dae9d184f26cb7a9c1644c2f795
clystone <1049267602@qq.com>
2017-07-10 16:52:48 +0800

    左移完成

--- 2048main.js
+++ 2048main.js
@@ -8,24 +8,26 @@ $(document).ready(function () {
     newgame();
 });
 
-function newgame(){
+function newgame() {
     //初始化棋盘格
     init();
     //在随机的两个格子生成数字
+    generateOneNumber();
+    generateOneNumber();
 }
 
-function init(){
-    for (var i = 0; i < 4; i++){
-        for (var j = 0; j < 4; j++){
-            var girdCell = $("#gird-cell-" + i + "-" + j);
-            girdCell.css("top",getPosTop(i,j));
-            girdCell.css("left",getPosLeft(i,j));
+function init() {
+    for (var i = 0; i < 4; i++) {
+        for (var j = 0; j < 4; j++) {
+            var gridCell = $("#grid-cell-" + i + "-" + j);
+            gridCell.css("top", getPosTop(i, j));
+            gridCell.css("left", getPosLeft(i, j));
         }
     }
 
-    for(var i =0; i < 4; i++){
+    for (var i = 0; i < 4; i++) {
         board[i] = new Array();
-        for(var j = 0; j < 4; j++){
+        for (var j = 0; j < 4; j++) {
             board[i][j] = 0;
         }
     }
@@ -33,28 +35,118 @@ function init(){
     updateBoardView();
 }
 
-function updateBoardView(){
+function updateBoardView() {
     $(".number-cell").remove();
-    for (var i = 0;i < 4; i++){
+    for (var i = 0; i < 4; i++)
         for (var j = 0; j < 4; j++) {
-            $("#gird-container").append('<div class="number-cell" id="number-ceil-'+i+'-'+j+'"></div>');
-            var theNumberCell = $('#number-cell-'+i+'-'+j);
+            $("#grid-container").append('<div class="number-cell" id="number-cell-' + i + '-' + j + '"></div>');
+            var theNumberCell = $('#number-cell-' + i + '-' + j);
 
-            if (board[i][j] === 0){
-                theNumberCell.css('width','0px');
-                theNumberCell.css('height','0px');
-                theNumberCell.css('top',getPosTop(i,j)+50);
-                theNumberCell.css('left',getPosLeft(i,j)+50);
+            if (board[i][j] === 0) {
+                theNumberCell.css('width', '0px');
+                theNumberCell.css('height', '0px');
+                theNumberCell.css('top', getPosTop(i, j) + 50);
+                theNumberCell.css('left', getPosLeft(i, j) + 50);
             }
-            else{
-                theNumberCell.css('width','100px');
-                theNumberCell.css('height','100px');
-                theNumberCell.css('top',getPosTop(i,j));
-                theNumberCell.css('left',getPosLeft(i,j));
-                theNumberCell.css('background-color',getNumberBackgroundColor(board[i][j]));
-                theNumberCell.css('color',getNumberColor(board[i][j]));
-                theNumberCell.test(board[i][j]);
+            else {
+                theNumberCell.css('width', '100px');
+                theNumberCell.css('height', '100px');
+                theNumberCell.css('top', getPosTop(i, j));
+                theNumberCell.css('left', getPosLeft(i, j));
+                theNumberCell.css('background-color', getNumberBackgroundColor(board[i][j]));
+                theNumberCell.css('color', getNumberColor(board[i][j]));
+                theNumberCell.text(board[i][j]);
+            }
+        }
+
+}
+
+function generateOneNumber() {
+    if (nospace(board)) {
+        return false;
+    }
+    //随机一个位置
+    var randx = parseInt(Math.floor(Math.random() * 4));
+    var randy = parseInt(Math.floor(Math.random() * 4));
+    while (true) {
+        if (board[randx][randy] === 0) {
+            break;
+        }
+        randx = parseInt(Math.floor(Math.random() * 4));
+        randy = parseInt(Math.floor(Math.random() * 4));
+    }
+    //随机一个数字
+    var ranNumber1 = Math.random() < 0.5 ? 2 : 4;
+    //在随机位置显示随机数字
+    board[randx][randy] = ranNumber1;
+    showNumberWithAnimation(randx, randy, ranNumber1);
+    return true;
+}
+
+$(document).keydown(function (event) {
+    switch (event.keyCode) {
+        case 37: //left
+            if (moveLeft()) {
+                generateOneNumber();
+                isgameover();
+            }
+            break;
+        case 38: //up
+            if (moveUp()) {
+                generateOneNumber();
+                isgameover();
+            }
+            break;
+        case 39: //right
+            if (moveRight()) {
+                generateOneNumber();
+                isgameover();
+            }
+            break;
+        case 40: //down
+            if (moveDown()) {
+                generateOneNumber();
+                isgameover();
+            }
+            break;
+        default: //default
+            break;
+    }
+});
+
+function isgameover(){
+
+}
+
+function moveLeft() {
+    if (!canMoveLeft(board)) {
+        return false;
+    }
+    //moveLeft
+    for (var i = 0; i < 4; i++) {
+        for (var j = 1; j < 4; j++) {
+            if (board[i][j] != 0) {
+                for (var k = 0; k < j; k++) {
+                    if (board[i][k] == 0 && noBlockHorizontal(i, k, j, board)) {
+                        //move
+                        showMoveAnimation(i,j, i,k);
+                        board[i][k] = board[i][j];
+                        board[i][j] = 0;
+                        continue;
+                    }
+                    else if(board[i][k] == board[i][j] && noBlockHorizontal(i, k, j, board)){
+                        //move
+                        showMoveAnimation(i,j, i,k);
+                        //add
+                        board[i][k] += board[i][j];
+                        board[i][j] = 0;
+
+                        continue;
+                    }
+                }
             }
         }
     }
+    setTimeout("updateBoardView()",200);
+    return true;
 }
